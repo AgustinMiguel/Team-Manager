c8a3dbba5593d71d85e01e6387e4e9ab3bbcdf25
Agustin Miguel <agustin.miguel@earndlt.com>
2024-03-20 14:25:38 -0300

    Prisma added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger("bootstrap");
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
   await app.listen(3000);
+  logger.log(`Application ready on port 3000`);
 }
 bootstrap();
